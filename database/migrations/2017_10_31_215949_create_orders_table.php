95870ac11281b242ca995d6814af1dd8807b044b
Logan <logan@sirdevan.com>
2017-10-31 18:30:25 -0400

    Create init db migrations | Orders & Products

--- database/migrations/2017_10_31_215949_create_orders_table.php
+++ database/migrations/2017_10_31_215949_create_orders_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Product extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,10 @@ class Product extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +25,6 @@ class Product extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('orders');
     }
 }
