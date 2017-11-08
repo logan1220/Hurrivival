c5ae9f661bbecb76475ce39861f505ed7d9ebcf3
Logan <logan@sirdevan.com>
2017-11-08 09:36:02 -0500

    User and Product model updates

--- app/Product.php
+++ app/Product.php
@@ -4,7 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Product
+ *
+ * @property int $product_id
+ * @property string $product_name
+ * @property string $product_total_quantity
+ * @property string $product_sku
+ * @property string $price
+ * @property string $category_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereProductName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereProductSku($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereProductTotalQuantity($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'product_name',
+        'product_total_quantity',
+        'product_sku',
+        'price',
+        'category_id',
+    ];
 }
--- app/User.php
+++ app/User.php
@@ -10,6 +10,39 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string|null $permissions
+ * @property string $phone
+ * @property string $address1
+ * @property string $address2
+ * @property string $city
+ * @property string $state
+ * @property string $zip
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereAddress1($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereAddress2($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePermissions($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereZip($value)
+ * @mixin \Eloquent
+ */
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
